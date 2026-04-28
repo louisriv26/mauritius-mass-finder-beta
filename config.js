29fefe4c2fcccbdc414ed4950e3f5be0de02bc7c
louisriv26 <louis.rivalland@gmail.com>
2026-04-28 17:58:27 +0400

    Update to v24.8

---
+++ config.js
@@ -0,0 +1,13 @@
+// Mauritius Mass Finder v24.8 configuration
+window.MASS_FINDER_CONFIG = window.MASS_FINDER_CONFIG || {
+  DATA_JSON_URL: './data/masses.json',
+  GOOGLE_SHEET_CSV_URL: '',
+  REPORT_EMAIL: 'mauritiusmassfinder@gmail.com',
+  analytics: {
+    enabled: true,
+    provider: 'plausible',
+    siteId: 'louisriv26.github.io',
+    endpoint: 'https://plausible.io/js/script.js'
+  }
+};
+window.MMF_CONFIG = window.MASS_FINDER_CONFIG;
