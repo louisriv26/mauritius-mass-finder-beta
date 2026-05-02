402b95f3ba4fd8b72539ee5d342a360415998b64
louisriv26 <louis.rivalland@gmail.com>
2026-05-02 19:43:29 +0400

    v24.23.1

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// Mauritius Mass Finder v24.20.4 configuration
+// Mauritius Mass Finder v24.22.0 configuration
 window.MASS_FINDER_CONFIG = window.MASS_FINDER_CONFIG || {
   DATA_JSON_URL: './data/masses.json',
   GOOGLE_SHEET_CSV_URL: '',
